efda2c3f2e86373b07f7859d272fd9937e0e33d5
LukasFrankenQ <lukas.b.franken@gmail.com>
2022-08-31 23:12:50 +0100

    added extraction code and heat demand

---
+++ src/utils/__init__.py
@@ -0,0 +1,7 @@
+from .IdealDataInterface import IdealDataInterface
+from .IdealMetadataInterface import IdealMetadataInterface
+
+__all__ = [
+    'IdealDataInterface',
+    'IdealMetadataInterface',
+]
